244f5a53be97de5d830c9b0870b386734f65149e
LuisNeto020 <94919641+LuisNeto020@users.noreply.github.com>
2022-11-20 22:38:20 +0000

    Delete Meta 3/testes directory

--- Meta 3/testes/MethodInvocation.java
+++
@@ -1,9 +0,0 @@
-class Sum {
-    public static int operators(int n) {
-      return n+1;
-    }
-
-    public static void main(String[] args) {
-        operators(operators(operators(operators(operators(1)))));
-    }
-}
--- Meta 3/testes/printing_void.java
+++
@@ -1,9 +0,0 @@
-class Error {
-	public static void a(int b) {
-		b = b + 2;
-		return;
-	}
-    public static void main(String[] args) {
-        System.out.print(a(3));
-    }
-}
